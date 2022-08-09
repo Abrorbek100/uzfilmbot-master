336917456f84032521ba29a246b1dfc7a667ea3d
Abrorbek <abrorbek.axmadjanov810@gmail.com>
2022-08-09 17:56:33 +0500

    kncaskc

--- keyboards/inline/subscription.py
+++ keyboards/inline/subscription.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 check_button = InlineKeyboardMarkup(
     inline_keyboard=[[
-        InlineKeyboardButton(text="Obunani bo'lish", url="https://t.me/+iON74y-LQoliOWVi"),
+        InlineKeyboardButton(text="Obunani bo'lish", url="https://t.me/myeterniiity"),
 
         InlineKeyboardButton(text="Obunani tekshirish ✅", callback_data="check_subs")
     ]]
